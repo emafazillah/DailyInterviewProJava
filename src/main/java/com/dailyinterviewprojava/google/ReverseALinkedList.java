79c4ab43900d6350a1f9cf038c2340435ae6f9ac
Ema Fazillah Abu Bakar <gatsu2811@gmail.com>
2019-08-01 17:31:08 +0800

    Reverse a Linked List 2nd commit

--- src/main/java/com/dailyinterviewprojava/google/ReverseALinkedList.java
+++ src/main/java/com/dailyinterviewprojava/google/ReverseALinkedList.java
@@ -1,30 +1,56 @@
 package com.dailyinterviewprojava.google;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class ReverseALinkedList {
 	
 	public static void main(String...strings) {
-		// Input
+		// Input to linked list
 		Scanner scanner = new Scanner(System.in);
 		String inputs = scanner.next();
 		inputs = inputs.replace("[", "");
 		inputs = inputs.replace("]", "");
-		
-		
+		String[] inputArray = inputs.split(",");
 		ListNode node = null;
+		for(String input : inputArray) {
+			if(!"null".equals(input)) {
+				node = insertListNode(node, Integer.parseInt(input));
+			}
+		}
+		
+		// Reverse linked list
+		node = reverseList(node);
 		
 		scanner.close();
 	}
 	
+	static ListNode insertListNode(ListNode listNode, int value) {
+		if(listNode == null) {
+			listNode = new ListNode(value);
+		} else {
+			ListNode temp = listNode;
+			listNode = new ListNode(value);
+			listNode.next = temp;
+		}
+		
+		return listNode;
+	}
+	
 	
 	static ListNode reverseList(ListNode listNode) {
+		if(listNode == null) {
+			return null;
+		}
+		
+		Stack<ListNode> stack = new Stack<ListNode>();
+		while(listNode != null) {
+			ListNode temp = listNode;
+			
+		}
 		
 		
-		
-		
-		// TODO
-		return null;
+		return listNode;
 	}
 
 }
