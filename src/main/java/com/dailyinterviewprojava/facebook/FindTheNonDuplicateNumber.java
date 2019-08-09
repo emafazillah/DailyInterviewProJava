d905d3249e1d4a933727c6647a9386a849130d25
Ema Fazillah Abu Bakar <gatsu2811@gmail.com>
2019-08-09 17:42:23 +0800

    Find the non-duplicate number.

---
+++ src/main/java/com/dailyinterviewprojava/facebook/FindTheNonDuplicateNumber.java
@@ -0,0 +1,61 @@
+package com.dailyinterviewprojava.facebook;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * 
+ * Given a list of numbers, where every number shows up twice except for one number, find that one number.
+ * Example:
+ * Input: [4, 3, 2, 4, 1, 3, 2]
+ * Output: 1
+ *
+ */
+public class FindTheNonDuplicateNumber {
+	
+	public static void main(String...strings) {
+		// Input into array
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.next();
+		input = input.replace("[", "");
+		input = input.replace("]", "");
+		String[] inputs = input.split(",");
+		int[] nums = new int[inputs.length];
+		for (int i = 0; i < inputs.length; i++) {
+			nums[i] = Integer.parseInt(inputs[i]);
+		}
+		
+		// Print output
+		System.out.println(singleNumber(nums));
+		
+		scanner.close();
+	}
+	
+	static int singleNumber(int[] nums) {
+		// Count duplicate
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			if (i == 0) {
+				map.put(nums[i], 1);
+			} else {
+				if (map.containsKey(nums[i])) {
+					int val = map.get(nums[i]);
+					map.put(nums[i], ++val);
+				} else {
+					map.put(nums[i], 1);
+				}
+			}
+		}
+		
+		// Get non-duplicate
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			if (entry.getValue() == 1) {
+				return entry.getKey();
+			}
+		}
+		
+		return 0;
+	}
+
+}
