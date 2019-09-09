1f4cb9668a3602a317f33e1479c4b1d53c249008
emafazillah <gatsu2811@gmail.com>
2019-09-10 00:43:08 +0800

    Update Word Search solution and add new question

---
+++ src/main/java/com/dailyinterviewprojava/amazon/LongestSubstringWithKDistinctCharacters.java
@@ -0,0 +1,14 @@
+package com.dailyinterviewprojava.amazon;
+
+/**
+ * 
+ * @author ema
+ * You are given a string s, and an integer k. Return the length of the longest substring in s that contains at most k distinct characters.
+ * 
+ * For instance, given the string:
+ * aabcdefff and k = 3, then the longest substring with 3 distinct characters would be defff. The answer should be 5.
+ * 
+ */
+public class LongestSubstringWithKDistinctCharacters {
+
+}
