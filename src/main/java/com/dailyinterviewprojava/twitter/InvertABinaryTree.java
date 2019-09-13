b31b5a4b94fb2075a3d74d4f0b83846551723d8e
emafazillah <gatsu2811@gmail.com>
2019-09-14 01:20:03 +0800

    Invert a Binary Tree

--- src/main/java/com/dailyinterviewprojava/twitter/InvertABinaryTree.java
+++ src/main/java/com/dailyinterviewprojava/twitter/InvertABinaryTree.java
@@ -1,5 +1,12 @@
 package com.dailyinterviewprojava.twitter;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+import com.dailyinterviewprojava.util.InputUtil;
+import com.dailyinterviewprojava.util.TreeNode;
+
 /**
  * 
  * @author ema
@@ -24,5 +31,54 @@ package com.dailyinterviewprojava.twitter;
  *
  */
 public class InvertABinaryTree {
+	
+	public static void main(String...strings) {
+		// Insert input into TreeNode
+		Scanner scanner = new Scanner(System.in);
+		String[] inputArr = InputUtil.inputArr(scanner.next());
+		TreeNode root = null;
+		for(String input : inputArr) {
+			root = insertTreeNode(root, Integer.parseInt(input));
+		}
+		
+		// Invert tree
+		root = invertTree(root);
+		
+		scanner.close();
+	}
+	
+	static TreeNode insertTreeNode(TreeNode root, int input) {
+		if(root == null) {
+			root = new TreeNode(input);
+		} else {
+			if(input > root.val) {
+				root.right = insertTreeNode(root.right, input);
+			} else {
+				root.left = insertTreeNode(root.left, input);
+			}
+		}
+		
+		return root;
+	}
+	
+	static TreeNode invertTree(TreeNode root) {
+		if(root == null) {
+			return null;
+		}
+		
+	    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+	    queue.add(root);
+	    
+	    while(!queue.isEmpty()) {
+	        TreeNode current = queue.poll();
+	        TreeNode temp = current.left;
+	        current.left = current.right;
+	        current.right = temp;
+	        if (current.left != null) queue.add(current.left);
+	        if (current.right != null) queue.add(current.right);
+	    }
+	    
+	    return root;
+    }
 
 }
