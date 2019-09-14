c7d3dbab299162ceeb97a6372ff53af9cfa3f735
emafazillah <gatsu2811@gmail.com>
2019-09-15 00:06:44 +0800

    Update new question

---
+++ src/main/java/com/dailyinterviewprojava/twitter/ThreeSum.java
@@ -0,0 +1,16 @@
+package com.dailyinterviewprojava.twitter;
+
+/**
+ * 
+ * @author ema
+ * Given an array, nums, of n integers, find all unique triplets (three numbers, a, b, & c) in nums such that a + b + c = 0. 
+ * Note that there may not be any triplets that sum to zero in nums, and that the triplets must not be duplicates.
+ * 
+ * Example:
+ * Input: nums = [0, -1, 2, -3, 1]
+ * Output: [0, -1, 1], [2, -3, 1]
+ * 
+ */
+public class ThreeSum {
+
+}
