c1b82680ebfa35741e2693c215e57a01b89a31cf
emafazillah <gatsu2811@gmail.com>
2019-09-20 03:20:14 +0800

    Add new question

---
+++ src/main/java/com/dailyinterviewprojava/google/RunningMedian.java
@@ -0,0 +1,13 @@
+package com.dailyinterviewprojava.google;
+
+/**
+ * 
+ * @author ema
+ * You are given a stream of numbers. Compute the median for each new element .
+ * 
+ * Eg. Given [2, 1, 4, 7, 2, 0, 5], the algorithm should output [2, 1.5, 2, 3.0, 2, 2, 2]
+ * 
+ */
+public class RunningMedian {
+
+}
