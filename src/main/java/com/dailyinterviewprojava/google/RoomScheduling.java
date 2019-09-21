aa83565c45fc4cdd25f819cf58e696ca611505a2
emafazillah <gatsu2811@gmail.com>
2019-09-22 01:04:00 +0800

    Update questions

---
+++ src/main/java/com/dailyinterviewprojava/google/RoomScheduling.java
@@ -0,0 +1,14 @@
+package com.dailyinterviewprojava.google;
+
+/**
+ * 
+ * @author ema
+ * You are given an array of tuples (start, end) representing time intervals for lectures. 
+ * The intervals may be overlapping. Return the number of rooms that are required.
+ * 
+ * For example. [(30, 75), (0, 50), (60, 150)] should return 2.
+ * 
+ */
+public class RoomScheduling {
+
+}
