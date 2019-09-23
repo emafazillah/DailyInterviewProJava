c221166d84d520fc1a1a0256abf29340254ea981
emafazillah <gatsu2811@gmail.com>
2019-09-23 23:53:58 +0800

    Update questions

---
+++ src/main/java/com/dailyinterviewprojava/amazon/ProductOfArrayExceptSelf.java
@@ -0,0 +1,16 @@
+package com.dailyinterviewprojava.amazon;
+
+/**
+ * 
+ * @author ema
+ * You are given an array of integers. Return an array of the same size where the element at each index is 
+ * the product of all the elements in the original array except for the element at that index.
+ * 
+ * For example, an input of [1, 2, 3, 4, 5] should return [120, 60, 40, 30, 24].
+ * 
+ * You cannot use division in this problem.
+ *
+ */
+public class ProductOfArrayExceptSelf {
+
+}
---
+++ src/main/java/com/dailyinterviewprojava/facebook/MergeListOfNumberIntoRanges.java
@@ -0,0 +1,16 @@
+package com.dailyinterviewprojava.facebook;
+
+/**
+ * 
+ * @author ema
+ * Given a sorted list of numbers, return a list of strings that represent all of the consecutive numbers.
+ * 
+ * Example:
+ * Input: [0, 1, 2, 5, 7, 8, 9, 9, 10, 11, 15]
+ * Output: ['0->2', '5->5', '7->11', '15->15']
+ * Assume that all numbers will be greater than or equal to 0, and each element can repeat.
+ * 
+ */
+public class MergeListOfNumberIntoRanges {
+
+}
