31f2e94c95fdf243cfa5be3711898760f897cea5
emafazillah <gatsu2811@gmail.com>
2019-09-29 04:04:45 +0800

    Contiguous Subarray with Maximum Sum

---
+++ src/main/java/com/dailyinterviewprojava/apple/TreeSerialization.java
@@ -0,0 +1,14 @@
+package com.dailyinterviewprojava.apple;
+
+/**
+ * 
+ * @author ema
+ * You are given the root of a binary tree. You need to implement 2 functions:
+ * 
+ * 1. serialize(root) which serializes the tree into a string representation
+ * 2. deserialize(s) which deserializes the string back to the original tree that it represents
+ *
+ */
+public class TreeSerialization {
+
+}
--- src/main/java/com/dailyinterviewprojava/twitter/ContiguousSubarrayWithMaximumSum.java
+++ src/main/java/com/dailyinterviewprojava/twitter/ContiguousSubarrayWithMaximumSum.java
@@ -1,5 +1,9 @@
 package com.dailyinterviewprojava.twitter;
 
+import java.util.Scanner;
+
+import com.dailyinterviewprojava.util.InputUtil;
+
 /**
  * 
  * @author ema
@@ -15,5 +19,36 @@ package com.dailyinterviewprojava.twitter;
  *
  */
 public class ContiguousSubarrayWithMaximumSum {
+	
+	public static void main(String...strings) {
+		// Input
+		Scanner scanner = new Scanner(System.in);
+		String[] inputs = InputUtil.inputArr(scanner.next());
+		int[] nums = InputUtil.integerArr(inputs);
+		
+		// Print output
+		System.out.println(maxSubArraySum(nums));
+		
+		scanner.close(); 
+	}
+	
+	static int maxSubArraySum(int[] nums) {
+		int maxCount = nums[0];
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			count += nums[i];
+			
+			if (count <= 0) {
+				maxCount = Math.max(count, maxCount);
+				count = 0;
+				continue;
+			}
+			
+			if (count > maxCount) {
+				maxCount = count; 
+			}
+		}
+        return maxCount;
+    }
 
 }
