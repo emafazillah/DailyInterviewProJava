c52997c1e60d7c1261cc65f5228df2febf9510b7
emafazillah <gatsu2811@gmail.com>
2019-09-28 14:06:25 +0800

    Update questions

---
+++ src/main/java/com/dailyinterviewprojava/apple/ArithmeticBinaryTree.java
@@ -0,0 +1,25 @@
+package com.dailyinterviewprojava.apple;
+
+/**
+ * 
+ * @author ema
+ * Hi, here's your problem today. This problem was recently asked by Apple:
+ * 
+ * You are given a binary tree representation of an arithmetic expression. In this tree, each leaf is an integer value,, and a non-leaf node is one of the four operations: '+', '-', '*', or '/'.
+ * 
+ * Write a function that takes this tree and evaluates the expression.
+ * 
+ * Example:
+ * 
+ *        *
+ *       / \
+ *      +    +
+ *     / \  / \
+ *    3  2  4  5
+ *    
+ * This is a representation of the expression (3 + 2) * (4 + 5), and should return 45.
+ *
+ */
+public class ArithmeticBinaryTree {
+
+}
---
+++ src/main/java/com/dailyinterviewprojava/microsoft/AnglesOfAClock.java
@@ -0,0 +1,11 @@
+package com.dailyinterviewprojava.microsoft;
+
+/**
+ * 
+ * @author ema
+ * Given a time in the format of hour and minute, calculate the angle of the hour and minute hand on a clock
+ *
+ */
+public class AnglesOfAClock {
+
+}
