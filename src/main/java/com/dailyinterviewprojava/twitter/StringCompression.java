cc97a84180df6cffe562f046485829e478ff6dc3
emafazillah <gatsu2811@gmail.com>
2019-10-13 23:43:50 +0800

    Add new question

---
+++ src/main/java/com/dailyinterviewprojava/twitter/StringCompression.java
@@ -0,0 +1,15 @@
+package com.dailyinterviewprojava.twitter;
+
+/**
+ * 
+ * @author ema
+ * Given an array of characters with repeats, compress it in place. The length after compression should be less than or equal to the original array.
+ * 
+ * Example:
+ * Input: ['a', 'a', 'b', 'c', 'c', 'c']
+ * Output: ['a', '2', 'b', 'c', '3']
+ * 
+ */
+public class StringCompression {
+
+}
