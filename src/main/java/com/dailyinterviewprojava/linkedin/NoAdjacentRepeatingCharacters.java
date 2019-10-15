e17757dbc9630ba3c565348881fb2959c69930da
emafazillah <gatsu2811@gmail.com>
2019-10-16 02:52:18 +0800

    Add new questions

---
+++ src/main/java/com/dailyinterviewprojava/linkedin/NoAdjacentRepeatingCharacters.java
@@ -0,0 +1,15 @@
+package com.dailyinterviewprojava.linkedin;
+
+/**
+ * 
+ * @author ema
+ * Given a string, rearrange the string so that no character next to each other are the same. If no such arrangement is possible, then return None.
+ * 
+ * Example:
+ * Input: abbccc
+ * Output: cbcbca
+ * 
+ */
+public class NoAdjacentRepeatingCharacters {
+
+}
---
+++ src/main/java/com/dailyinterviewprojava/uber/MakeTheLargestNumber.java
@@ -0,0 +1,15 @@
+package com.dailyinterviewprojava.uber;
+
+/**
+ * 
+ * @author ema
+ * Given a number of integers, combine them so it would create the largest number.
+ * 
+ * Example:
+ * Input:  [17, 7, 2, 45, 72]
+ * Output:  77245217
+ * 
+ */
+public class MakeTheLargestNumber {
+
+}
