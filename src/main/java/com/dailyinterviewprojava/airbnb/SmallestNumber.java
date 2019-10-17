4950cbc5b0914d13b8d18e5b2896b3e77d7a958e
emafazillah <gatsu2811@gmail.com>
2019-10-18 02:34:05 +0800

    Add new questions

---
+++ src/main/java/com/dailyinterviewprojava/airbnb/SmallestNumber.java
@@ -0,0 +1,16 @@
+package com.dailyinterviewprojava.airbnb;
+
+/**
+ * 
+ * @author ema
+ * Given a sorted list of positive numbers, find the smallest positive number that cannot be a sum of any subset in the list.
+ * 
+ * Example:
+ * Input: [1, 2, 3, 8, 9, 10]
+ * Output: 7
+ * Numbers 1 to 6 can all be summed by a subset of the list of numbers, but 7 cannot.
+ * 
+ */
+public class SmallestNumber {
+
+}
---
+++ src/main/java/com/dailyinterviewprojava/facebook/MaximumPathSumInBinaryTree.java
@@ -0,0 +1,12 @@
+package com.dailyinterviewprojava.facebook;
+
+/**
+ * 
+ * @author ema
+ * You are given the root of a binary tree. Find the path between 2 nodes that maximizes the sum of all the nodes in the path, and return the sum. 
+ * The path does not necessarily need to go through the root.
+ * 
+ */
+public class MaximumPathSumInBinaryTree {
+
+}
