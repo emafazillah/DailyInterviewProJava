9489301e7e5efd4ad07ea222c9451821bde4bbff
emafazillah <gatsu2811@gmail.com>
2019-10-18 23:35:32 +0800

    Add new question

---
+++ src/main/java/com/dailyinterviewprojava/facebook/PermutationsOfNumbers.java
@@ -0,0 +1,11 @@
+package com.dailyinterviewprojava.facebook;
+
+/**
+ * 
+ * @author ema
+ * You are given an array of integers. Return all the permutations of this array.
+ * 
+ */
+public class PermutationsOfNumbers {
+
+}
