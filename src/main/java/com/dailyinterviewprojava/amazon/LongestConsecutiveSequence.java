1aae66a133070fdfe1a2009c39e3681ae73066bd
emafazillah <gatsu2811@gmail.com>
2019-10-20 22:41:51 +0800

    Add new question

---
+++ src/main/java/com/dailyinterviewprojava/amazon/LongestConsecutiveSequence.java
@@ -0,0 +1,13 @@
+package com.dailyinterviewprojava.amazon;
+
+/**
+ * 
+ * @author ema
+ * You are given an array of integers. Return the length of the longest consecutive elements sequence in the array.
+ * 
+ * For example, the input array [100, 4, 200, 1, 3, 2] has the longest consecutive sequence 1, 2, 3, 4, and thus, you should return its length, 4.
+ * 
+ */
+public class LongestConsecutiveSequence {
+
+}
