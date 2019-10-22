768c736358a56720d64b297a958974951ba71af3
emafazillah <gatsu2811@gmail.com>
2019-10-22 23:55:54 +0800

    Add new questions

---
+++ src/main/java/com/dailyinterviewprojava/microsoft/LevelOrderTraversalOfBinaryTree.java
@@ -0,0 +1,19 @@
+package com.dailyinterviewprojava.microsoft;
+
+/**
+ * 
+ * @author ema
+ * Given the root of a binary tree, print its level-order traversal. For example:
+ * 
+	  1
+	 / \
+	2   3
+	   / \
+	  4   5
+ *
+ * The following tree should output 1, 2, 3, 4, 5.
+ *
+ */
+public class LevelOrderTraversalOfBinaryTree {
+
+}
---
+++ src/main/java/com/dailyinterviewprojava/microsoft/LinkedListIsPalindrome.java
@@ -0,0 +1,11 @@
+package com.dailyinterviewprojava.microsoft;
+
+/**
+ * 
+ * @author ema
+ * You are given a doubly linked list. Determine if it is a palindrome.
+ *
+ */
+public class LinkedListIsPalindrome {
+
+}
