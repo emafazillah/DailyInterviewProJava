3fa4436f7bea2c76767a67156b64762b5d8d0b88
emafazillah <gatsu2811@gmail.com>
2019-10-24 02:21:01 +0800

    Add new questions

---
+++ src/main/java/com/dailyinterviewprojava/amazon/SubarrayWithTargetSum.java
@@ -0,0 +1,12 @@
+package com.dailyinterviewprojava.amazon;
+
+/**
+ * 
+ * @author ema
+ * You are given an array of integers, and an integer K. Return the subarray which sums to K. 
+ * You can assume that a solution will always exist.
+ * 
+ */
+public class SubarrayWithTargetSum {
+
+}
---
+++ src/main/java/com/dailyinterviewprojava/apple/ShiftedString.java
@@ -0,0 +1,13 @@
+package com.dailyinterviewprojava.apple;
+
+/**
+ * 
+ * @author ema
+ * You are given two strings, A and B. Return whether A can be shifted some number of times to get B.
+ * 
+ * Eg. A = abcde, B = cdeab should return true because A can be shifted 3 times to the right to get B. A = abc and B= acb should return false.
+ * 
+ */
+public class ShiftedString {
+
+}
