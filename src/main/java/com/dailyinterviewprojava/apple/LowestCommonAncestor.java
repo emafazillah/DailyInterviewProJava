2b456e576cf33fec2e032ca73bd15a110bbfb4b0
emafazillah <gatsu2811@gmail.com>
2019-10-26 06:37:07 +0800

    Add new questions

---
+++ src/main/java/com/dailyinterviewprojava/apple/LowestCommonAncestor.java
@@ -0,0 +1,12 @@
+package com.dailyinterviewprojava.apple;
+
+/**
+ * 
+ * @author ema
+ * You are given the root of a binary tree, along with two nodes, A and B. 
+ * Find and return the lowest common ancestor of A and B. For this problem, you can assume that each node also has a pointer to its parent, along with its left and right child.
+ * 
+ */
+public class LowestCommonAncestor {
+
+}
---
+++ src/main/java/com/dailyinterviewprojava/twitter/BinaryTreeLevelWithMinimumSum.java
@@ -0,0 +1,13 @@
+package com.dailyinterviewprojava.twitter;
+
+/**
+ * 
+ * @author ema
+ * You are given the root of a binary tree. Find the level for the binary tree with the minimum sum, and return that value.
+ * 
+ * For instance, in the example below, the sums of the trees are 10, 2 + 8 = 10, and 4 + 1 + 2 = 7. So, the answer here should be 7.
+ *
+ */
+public class BinaryTreeLevelWithMinimumSum {
+
+}
