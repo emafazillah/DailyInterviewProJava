642b44ef7c81894aacde5cc1f1993e7426f0968e
emafazillah <gatsu2811@gmail.com>
2020-03-23 18:02:23 +0800

    Add Number of 1 bits solution.

--- src/main/java/com/dailyinterviewprojava/google/NumberOf1Bits.java
+++ src/main/java/com/dailyinterviewprojava/google/NumberOf1Bits.java
@@ -1,5 +1,7 @@
 package com.dailyinterviewprojava.google;
 
+import java.util.Scanner;
+
 /**
  * 
  * @author ema
@@ -7,5 +9,25 @@ package com.dailyinterviewprojava.google;
  * 
  */
 public class NumberOf1Bits {
+	
+	public static void main(String...strings) {
+		// Input
+		Scanner scanner = new Scanner(System.in);
+		int n = Integer.parseUnsignedInt(scanner.next(), 2);
+		
+		System.out.println(hammingWeight(n));
+		
+		scanner.close();
+	}
+	
+	// you need to treat n as an unsigned value
+    public static int hammingWeight(int n) { 
+    	int weight = 0;
+		for (int i = 0; i < 32; i++) {
+			if ((n & (1 << i)) != 0) 
+				weight++;
+		}
+		return weight;
+    }
 
 }
