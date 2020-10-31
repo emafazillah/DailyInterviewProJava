ff6f973c9742a0feca1228902d4c776055a8436c
Ema Fazillah Abu Bakar <gatsu2811@gmail.com>
2020-10-31 15:03:13 +0800

    Add solution for Palindrome Integers problem.

--- src/main/java/com/dailyinterviewprojava/twitter/PalindromeIntegers.java
+++ src/main/java/com/dailyinterviewprojava/twitter/PalindromeIntegers.java
@@ -1,5 +1,7 @@
 package com.dailyinterviewprojava.twitter;
 
+import java.util.Scanner;
+
 /**
  * 
  * @author ema
@@ -7,5 +9,30 @@ package com.dailyinterviewprojava.twitter;
  * 
  */
 public class PalindromeIntegers {
-
+    public static boolean isPalindrome(int x) {
+        int temp = x;
+		int reverse = 0;
+		
+		while(temp > 0) {
+			int remainder = temp % 10;
+			reverse = reverse * 10 + remainder;
+			temp = temp / 10;
+		}
+		
+		if (x == reverse) {
+			return true;
+		} else {
+			return false;
+		}
+    }
+    
+	public static void main(String... args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int input = scanner.nextInt();
+		boolean output = isPalindrome(input);
+		System.out.println(output);
+		
+		scanner.close();
+	}
 }
