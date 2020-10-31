fa6cb8cc62bee8c973bd2d8c4b4ec94ad3a8c9ca
Ratan Boddu <ratanboddu@gmail.com>
2020-10-31 14:51:33 +0530

    Adding Solution for airbnb - SmallestNumber

--- src/main/java/com/dailyinterviewprojava/airbnb/SmallestNumber.java
+++ src/main/java/com/dailyinterviewprojava/airbnb/SmallestNumber.java
@@ -1,4 +1,8 @@
 package com.dailyinterviewprojava.airbnb;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import com.dailyinterviewprojava.util.InputUtil;
 
 /**
  * 
@@ -12,5 +16,31 @@ package com.dailyinterviewprojava.airbnb;
  * 
  */
 public class SmallestNumber {
+    int findSmallestNumber(int arr[], int n)  
+        { 
+            int res = 1; // Initialize result 
+            for (int i = 0; i < n && arr[i] <= res; i++) 
+                res = res + arr[i]; 
+
+            return res; 
+        } 
+
+        public static void main(String[] args)  
+        { 
+            // Input
+            Scanner scanner = new Scanner(System.in);
+            String[] inputs = InputUtil.inputArr(scanner.next());
+            int[] nums = InputUtil.integerArr(inputs);
+
+            // Lenth of the array
+            int n1 = nums.length; 
+
+            // Printing Output
+            SmallestNumber small = new SmallestNumber(); 
+            System.out.println(small.findSmallestNumber(nums, n1));
+
+            scanner.close();
+
+        } 
 
 }
